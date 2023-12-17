758bab0584d0cffe4d716a791b244c7bfe00bfa5
Edmund15 <annohboateng15@gmail.com>
2023-12-17 17:40:27 +0000

    update 2

--- monty.c
+++ monty.c
@@ -1,4 +1,8 @@
 #include "monty.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * main - Entry point for the Monty interpreter.
@@ -12,28 +16,41 @@ int main(int argc, char *argv[])
     FILE *file;
     char *line = NULL;
     size_t len = 0;
+    int read;
     stack_t *stack = NULL;
     unsigned int line_number = 0;
+    int debug = 0;
 
-    if (argc != 2)
+    if (argc < 2 || argc > 3)
     {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
-    file = fopen(argv[1], "r");
+    if (argc == 3 && strcmp(argv[1], "-d") == 0)
+    {
+        debug = 1;
+        file = fopen(argv[2], "r");
+    }
+    else
+    {
+        file = fopen(argv[1], "r");
+    }
+
     if (file == NULL)
     {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        fprintf(stderr, "Error: Can't open file %s\n", (debug ? argv[2] : argv[1]));
         exit(EXIT_FAILURE);
     }
 
-    while (fgets(line, len, file) != NULL)
+    while ((read = getline(&line, &len, file)) != -1)
     {
         line_number++;
 
         if (line[0] == '#')
             continue;
+
+        line[strcspn(line, "\n")] = '\0';
 
         execute_opcode(line, &stack, line_number);
     }
@@ -44,3 +61,4 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 }
+
